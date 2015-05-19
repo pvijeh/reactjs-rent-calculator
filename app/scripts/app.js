7bc0cafb5169314a57ccb09ff3076f017f15a79b
Peter Vijeh <pvijeh@gmail.com>
2015-05-19 01:22:45 -0700

    editing components

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,26 +1,39 @@
 
 var React = window.React = require('react'),
+// include external components from ui folder like the exmaple blow: 
+//  Timer = require("./ui/Timer"),
     Timer = require("./ui/Timer"),
     mountNode = document.getElementById("app");
+
+    var obj = {
+      'row1' : {
+        'key1' : 'input1',
+        'key2' : 'input2' 
+      },
+      'row2' : {
+        'key3' : 'input3',
+        'key4' : 'input4'
+      }
+    };
 
 var TodoList = React.createClass({
   render: function() {
     var createItem = function(itemText) {
-      return <li>{itemText}</li>;
+      return <li>{itemText.thing} <input value={itemText.stuff}/></li>;
     };
     return <ul>{this.props.items.map(createItem)}</ul>;
   }
 });
 var TodoApp = React.createClass({
   getInitialState: function() {
-    return {items: [], text: ''};
+    return {items: [{thing: "stuff" , stuff: "100"},{thing: "stuff" , stuff: "100"}], text: ''};
   },
   onChange: function(e) {
     this.setState({text: e.target.value});
   },
   handleSubmit: function(e) {
     e.preventDefault();
-    var nextItems = this.state.items.concat([this.state.text]);
+    var nextItems = this.state.items.concat([{thing : this.state.text, stuff : ""}]);
     var nextText = '';
     this.setState({items: nextItems, text: nextText});
   },
@@ -29,16 +42,66 @@ var TodoApp = React.createClass({
       <div>
         <h3>TODO</h3>
         <TodoList items={this.state.items} />
-        <form onSubmit={this.handleSubmit}>
+       <div className="stuff"><p>stuff</p></div>
+       <div className="stuff">
+       <form onSubmit={this.handleSubmit}>
           <input onChange={this.onChange} value={this.state.text} />
-          <button>{'Add #' + (this.state.items.length + 1)}</button>
+          <button>Add</button>
         </form>
-        <Timer />
+        </div>
       </div>
     );
   }
 });
 
 
+
+
+
+
+
+
+
 React.render(<TodoApp />, mountNode);
+
+
+
+
+
+
+// var TodoList = React.createClass({
+//   render: function() {
+//     var createItem = function(itemText) {
+//       return <li>{itemText}</li>;
+//     };
+//     return <ul>{this.props.items.map(createItem)}</ul>;
+//   }
+// });
+// var TodoApp = React.createClass({
+//   getInitialState: function() {
+//     return {items: [{thing: "stuff" , stuff: "100"},{thing: "stuff" , stuff: "100"}], text: ''};
+//   },
+//   onChange: function(e) {
+//     this.setState({text: e.target.value});
+//   },
+//   handleSubmit: function(e) {
+//     e.preventDefault();
+//     var nextItems = this.state.items.concat([this.state.text]);
+//     var nextText = '';
+//     this.setState({items: nextItems, text: nextText});
+//   },
+//   render: function() {
+//     return (
+//       <div>
+//         <h3>TODO</h3>
+//         <TodoList items={this.state.items} />
+//         <form onSubmit={this.handleSubmit}>
+//           <input onChange={this.onChange} value={this.state.text} />
+//           <button>Add</button>
+//         </form>
+//         <Timer />
+//       </div>
+//     );
+//   }
+// });
 
